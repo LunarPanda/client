617aa7e3487dd1908372b3dca7c880e80741ff5d
t-hejian <t-hejian@microsoft.com>
2016-12-01 16:02:40 +0800

    eureka client

---
+++ src/main/java/hello/Bot.java
@@ -0,0 +1,47 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
+
+
+import com.google.code.chatterbotapi.ChatterBot;
+import com.google.code.chatterbotapi.ChatterBotFactory;
+import com.google.code.chatterbotapi.ChatterBotSession;
+import com.google.code.chatterbotapi.ChatterBotType;
+
+@Component
+@SessionScope
+public class Bot {
+	
+	ChatterBotSession botsession;
+	
+	@Value("${spring.application.name}")
+	private String appName;
+	
+	public Bot(){
+		ChatterBotFactory factory = new ChatterBotFactory();
+        ChatterBot bot=null;
+		try {
+			bot = factory.create(ChatterBotType.PANDORABOTS, "b0dafd24ee35a477");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        botsession = bot.createSession();
+	}
+	
+	String reply(String s){
+		String r="";
+		try {
+			r=botsession.think(s);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println();
+		return String.format("{\"content\":\"[%s] %s\"}",appName,r.replaceAll("\"", "\\\\\""));
+	}
+	
+}
--- src/main/java/hello/EurekaClientApplication.java
+++ src/main/java/hello/EurekaClientApplication.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,10 +28,35 @@ class ServiceInstanceRestController {
 
     @Autowired
     private DiscoveryClient discoveryClient;
+    
+    @Autowired
+    private Bot bot;
+    
+    @RequestMapping("/service")
+    public String home() {
+       return "hello from hello client.";
+    }
+    
+    @RequestMapping(value="/train",produces = "application/json")
+    public String train(){
+    	return "{\"accuracy\":0.975}";
+    }
+    
+    @RequestMapping(value="/chat",method=RequestMethod.GET,produces = "application/json")
+    public String chat(@RequestParam("request") String request){
+    	String reply=bot.reply(request);
+		return reply;
+    	
+    }
 
     @RequestMapping("/service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(
+    public String serviceInstancesByApplicationName(
             @PathVariable String applicationName) {
-        return this.discoveryClient.getInstances(applicationName);
+    	String result="hello: ";
+        List<String> list=this.discoveryClient.getServices();
+        for (String s: list){
+        	result+=s+" ";
+        }
+        return result;
     }
 }
